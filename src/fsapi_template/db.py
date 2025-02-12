0eb6e45e0eeb54b810bedb01db165a9277544f41
theocheng <zlz_gty@foxmail.com>
2025-02-12 20:01:45 +0800

    docs: add database library recommendations for SQL and NoSQL databases

---
+++ src/fsapi_template/db.py
@@ -0,0 +1,12 @@
+#
+#
+# 如果要使用 SQL 数据库
+# 最佳选择使 SQLModel， 这是 FastAPI 同作者的另一个项目
+# 可以和 FastAPI 完美整合
+# 文档： https://sqlmodel.tiangolo.com/
+
+#
+#
+# 如果使用 NoSQL 数据库，比如 MongoDB
+# 最佳的库是 Beanie
+# 文档： https://beanie-odm.dev/
